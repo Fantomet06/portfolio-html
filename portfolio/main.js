0dd49f2213b8cc087cb7af6fc5a87404c416fd38
Fantomet <Sigurd.Evensen@hotmail.com>
2021-12-21 20:00:24 +0100

    added custom model
    added custom model

--- portfolio/main.js
+++ portfolio/main.js
@@ -3,9 +3,10 @@ import './style.css'
 import * as THREE from 'three';
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer({
@@ -19,14 +20,20 @@ camera.position.setZ(30);
 const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
 const material = new THREE.MeshStandardMaterial( { color: 0xFF6347 } );
 const torus = new THREE.Mesh( geometry, material );
+const controls = new OrbitControls(camera, renderer.domElement);
 
 scene.add(torus)
 
 const pointLight = new THREE.PointLight(0xffffff)
 pointLight.position.set(0,0,0)
-
 const ambientLight = new THREE.AmbientLight(0xffffff)
 scene.add(pointLight, ambientLight)
+
+/*
+-----------
+| HELPERS |
+-----------
+*/
 
 //SHOWS LIGHT POSITIONS:
 const lightHelper = new THREE.PointLightHelper(pointLight)
@@ -36,7 +43,36 @@ scene.add(lightHelper)
 const gridHelper = new THREE.GridHelper(200, 50)
 scene.add(gridHelper)
 
-const controls = new OrbitControls(camera, renderer.domElement);
+/*
+-----------------
+| CUSTOM MODELS |
+-----------------
+*/
+
+const loader = new GLTFLoader();
+const draco = new DRACOLoader();
+draco.setDecoderConfig({ type: 'js' });
+draco.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/');
+loader.setDRACOLoader( draco );
+
+loader.load('models/scene.gltf', function ( gltf ) {
+    gltf.scene.scale.set(0.5,0.5,0.5);
+    gltf.scene.position.set(15, -53, -20)
+		scene.add( gltf.scene );
+	},
+	function ( xhr ) {
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+	},
+	function ( error ) {
+		console.log( 'An error happened' );
+	}
+);
+
+/*
+-------------
+| FUNCTIONS |
+-------------
+*/
 
 function addStar() {
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
@@ -48,7 +84,6 @@ function addStar() {
   star.position.set(x, y, z);
   scene.add(star)
 }
-
 Array(200).fill().forEach(addStar)
 
 function animate() {
@@ -61,4 +96,15 @@ function animate() {
   renderer.render( scene, camera );
 }
 
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
 animate()
+window.addEventListener( 'resize', onWindowResize );
+
