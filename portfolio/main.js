a08e5a2d5be5b5896dbc3e3bb1e3be615a61b03e
Fantomet <Sigurd.Evensen@hotmail.com>
2023-06-18 17:56:37 +0200

    asadf

--- portfolio/main.js
+++ portfolio/main.js
@@ -11,9 +11,8 @@ const raycaster = new THREE.Raycaster()
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector('#bg'),
-});
+const renderer = new THREE.WebGLRenderer();
+document.body.appendChild(renderer.domElement)
 
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight ); 
